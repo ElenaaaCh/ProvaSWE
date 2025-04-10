fbbcd7b19d382fe256894089e9b98cbe8e2ee632
EliaBergamin <eliberg031009@gmail.com>
2025-04-10 11:43:29 +0200

    change: format

--- 3Dataviz/backend/src/app.module.ts
+++ 3Dataviz/backend/src/app.module.ts
@@ -5,8 +5,8 @@ import { DataSourceModule } from "./modules/data-source/data-source.module";
 import { DataVisualizationModule } from "./modules/data-visualization/data-visualization.module";
 import { FetchersModule } from "./modules/fetchers/fetchers.module";
 import { ConfigModule } from "@nestjs/config";
-import { CacheModule } from './modules/cache/cache.module';
-import { CacheService } from './modules/cache/services/cache.service';
+import { CacheModule } from "./modules/cache/cache.module";
+import { CacheService } from "./modules/cache/services/cache.service";
 
 @Module({
   imports: [
